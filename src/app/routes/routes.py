5b4f4d4529382b3e2fe72c964d5c84612b78080b
Jota Feldmann <eusoujf@hotmail.com>
2020-01-30 03:36:51 -0300

    Remove old versions

--- src/app/routes/routes.py
+++ src/app/routes/routes.py
@@ -20,35 +20,6 @@ def _designate_cargo_for_truck_from_list(cargo, trucks_designated, closest_truck
 
     return distance, closest_truck
 
-# First version
-def _get_routes_simple(trucks: [Truck], cargos: [Cargo]) -> [ShortestRoute]:
-    for cargo in cargos:
-        closest_truck = None
-        distance = EARTH_MAX_DISTANCE_BETWEEN_TWO_POINTS
-
-        for truck in trucks:
-            cargo_distance_to_truck = get_distance(cargo.origin_location, truck.location)
-
-            if cargo_distance_to_truck < distance:
-                closest_truck = truck
-                distance = cargo_distance_to_truck
-
-        yield ShortestRoute(cargo, closest_truck, distance)
-
-# Second version, with a sorted list
-def _get_routes_simple_with_sorted_order(trucks: [Truck], cargos: [Cargo]) -> [ShortestRoute]:
-
-    for cargo in cargos:
-        closest_trucks_list = SortedDict()
-
-        for truck in trucks:
-            cargo_distance_to_truck = get_distance(cargo.origin_location, truck.location)
-            closest_trucks_list[cargo_distance_to_truck] = truck
-        
-        distance, closest_truck = closest_trucks_list.peekitem(0)
-        yield ShortestRoute(cargo, closest_truck, distance)
-
-# Third version, with max cargos
 def _get_routes_with_sorted_order_and_max_cargo_per_truck(trucks: [Truck], cargos: [Cargo], max_cargos_per_truck=1, get_distance=get_distance) -> [ShortestRoute]:
 
     trucks_designated = {}
