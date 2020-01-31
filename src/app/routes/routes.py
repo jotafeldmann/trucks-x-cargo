8dda0c27e43164cf89bb0aed89ed5b54e5621bfb
Jota Feldmann <eusoujf@hotmail.com>
2020-01-31 04:45:49 -0300

    Set legible name

--- src/app/routes/routes.py
+++ src/app/routes/routes.py
@@ -10,31 +10,33 @@ from app.routes.shortest_route import ShortestRoute
 from utils.geolocation import get_distance, EARTH_MAX_DISTANCE_BETWEEN_TWO_POINTS
 from utils.config import config
 
-def _get_routes_with_sorted_map(trucks: [Truck], cargos: [Cargo], max_cargos_per_truck=1, get_distance=get_distance) -> [ShortestRoute]:
+MAX_CARGOS_PER_TRUCK = 1
+
+def _get_routes_with_sorted_map(trucks: [Truck], cargos: [Cargo], max_cargos_per_truck = MAX_CARGOS_PER_TRUCK, get_distance=get_distance) -> [ShortestRoute]:
 
     trucks_designated = {}
 
     for cargo in cargos:
-        closest_trucks_ordered_iterable = SortedDict()
+        closest_trucks_sorted = SortedDict()
         cargo_truck_to_pick = 0
 
         for truck in trucks:
             cargo_distance_to_truck = get_distance(cargo.origin_location, truck.location)
-            closest_trucks_ordered_iterable[cargo_distance_to_truck] = truck
+            closest_trucks_sorted[cargo_distance_to_truck] = truck
         
-        distance, closest_truck = designate_cargo_for_truck(cargo, closest_trucks_ordered_iterable, cargo_truck_to_pick, max_cargos_per_truck)
+        distance, closest_truck = designate_cargo_for_truck(cargo, closest_trucks_sorted, cargo_truck_to_pick, max_cargos_per_truck)
 
         yield ShortestRoute(cargo, closest_truck, distance)
 
-def _get_routes_with_kdtree(trucks: TruckList, cargos: [Cargo], max_cargos_per_truck=1, get_distance=get_distance) -> [ShortestRoute]:
+def _get_routes_with_kdtree(trucks: TruckList, cargos: [Cargo], max_cargos_per_truck = MAX_CARGOS_PER_TRUCK, get_distance=get_distance) -> [ShortestRoute]:
 
     def lambda_get_closest_truck(map, index):
         return map[index]
 
     for cargo in cargos:
         cargo_truck_to_pick = 0
-        closest_trucks_ordered_iterable = trucks.get_closest_trucks_to_location(location=cargo.origin_location)
-        distance, closest_truck = designate_cargo_for_truck(cargo, closest_trucks_ordered_iterable, cargo_truck_to_pick, max_cargos_per_truck, lambda_get_closest_truck=lambda_get_closest_truck)
+        closest_trucks_sorted = trucks.get_closest_trucks_to_location(location=cargo.origin_location)
+        distance, closest_truck = designate_cargo_for_truck(cargo, closest_trucks_sorted, cargo_truck_to_pick, max_cargos_per_truck, lambda_get_closest_truck=lambda_get_closest_truck)
 
         yield ShortestRoute(cargo, closest_truck, distance)
 
