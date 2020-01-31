2ea2571b4c7ecf6d84bee02e34398ef11a2b3810
Jota Feldmann <eusoujf@hotmail.com>
2020-01-31 01:18:19 -0300

    Add map todo about imutable maps

--- src/app/routes/designate_cargo_for_truck.py
+++ src/app/routes/designate_cargo_for_truck.py
@@ -6,6 +6,7 @@ def designate_cargo_for_truck(cargo, closest_trucks_map = SortedDict(), cargo_tr
     if trucks_designated_map.get(closest_truck):
 
         if len(trucks_designated_map[closest_truck].get('cargos').keys()) == max_cargos_per_truck:
+            # TODO: work with imutable maps (new maps for each iteration)
             return designate_cargo_for_truck(cargo, closest_trucks_map, cargo_truck_to_pick + 1, max_cargos_per_truck, trucks_designated_map)
         
         trucks_designated_map[closest_truck].get('cargos').setdefault(cargo, distance)
