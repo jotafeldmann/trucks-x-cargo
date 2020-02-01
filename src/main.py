9b08fee9b3bea67e95c33e6ab65c0209bd16a36b
Jota Feldmann <eusoujf@hotmail.com>
2020-02-01 06:18:13 -0300

    Add title test

--- src/main.py
+++ src/main.py
@@ -8,6 +8,12 @@ from time import time
 from utils.config import config
 from utils.input import get_trucks_data, get_cargos_data
 
+DEFAULT_OUTPUT = print
+
+
+def output_columns_title(output):
+    return output("Cargo, Truck Company, Distance (km)")
+
 
 def load_data(get_trucks_data=get_trucks_data, get_cargos_data=get_cargos_data):
     trucks = TruckList()
@@ -16,16 +22,21 @@ def load_data(get_trucks_data=get_trucks_data, get_cargos_data=get_cargos_data):
     return trucks, cargos
 
 
-def output_routes(routes, output=print):
-    output('Cargo, Truck Company, Distance (km)')
+def output_routes(routes, output):
     return [[output(route.cargo.product + ', ', route.closest_truck.company + ', ', route.distance)]
             for route in routes]
 
 
-def main(load_data=load_data, get_routes=get_routes, output_routes=output_routes):
+def main(
+        load_data=load_data,
+        get_routes=get_routes,
+        output_routes=output_routes,
+        output_columns_title=output_columns_title,
+        output=DEFAULT_OUTPUT):
     trucks, cargos = load_data()
     routes = get_routes(trucks, cargos)
-    return output_routes(routes)
+    output_columns_title(output=output)
+    return output_routes(routes, output=output)
 
 
 if __name__ == "__main__":
--- src/tests/integration_tests/test_main.py
+++ src/tests/integration_tests/test_main.py
@@ -2,11 +2,15 @@ from unittest import TestCase
 from functools import partial
 from json import dumps
 
-from main import main, load_data, output_routes
+from main import main, load_data, output_routes, output_columns_title
 from tests.fixtures.load_fixtures import cargos_fixture_generator, trucks_fixture_generator
 
 
-def receive_return(*value):
+def receive_return(a=None, b=None, c=None):
+    return a, b, c
+
+
+def receive_return_single(value):
     return value
 
 
@@ -15,11 +19,18 @@ class TestMain(TestCase):
         self.load_data = partial(load_data,
                                  get_trucks_data=trucks_fixture_generator,
                                  get_cargos_data=cargos_fixture_generator)
-        self.output_routes = partial(output_routes, output=receive_return)
+        self.output_routes = output_routes
+        self.output_columns_title = output_columns_title
 
     def test_main(self):
-
-        routes = dumps(main(load_data=self.load_data, output_routes=self.output_routes))
+        title = self.output_columns_title(output=receive_return_single)
+        routes = dumps(
+            main(
+                load_data=self.load_data,
+                output_routes=output_routes,
+                output_columns_title=self.output_columns_title,
+                output=receive_return))
+        expected_title = "Cargo, Truck Company, Distance (km)"
         expected_routes = dumps([
             [["Light bulbs, ", "Viking Products Of Austin Incustin, ", 190.0]],
             [["Recyclables, ", "Ricardo Juradoacramento, ", 173.0]],
@@ -29,4 +40,5 @@ class TestMain(TestCase):
             [["Wood, ", "Gary Lee Wilcoxpencer, ", 348.0]],
             [["Oranges, ", "Fish-Bones Towingew York, ", 260.0]]
         ])
+        self.assertEqual(title, expected_title)
         self.assertEqual(routes, expected_routes)
