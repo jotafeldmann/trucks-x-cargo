eeb42c1adaba9bdca51cb78bd3b97a2c6cf6b2e5
Jota Feldmann <eusoujf@hotmail.com>
2020-02-02 04:36:20 -0300

    Add files path option

--- src/utils/config.py
+++ src/utils/config.py
@@ -11,6 +11,7 @@ class Config:
     def __init__(self):
         parser = argparse.ArgumentParser("make run")
         parser.add_argument("--algorithm", required=False, help="Use kdtree algorithm")
+        parser.add_argument("--cargos-csv", required=False, help="The cargos CSV file path")
         parser.add_argument("--debug", required=False, help="Use debug features")
         parser.add_argument("--max-cargos", required=False, help="Max cargos per truck, default 1")
         parser.add_argument(
@@ -18,6 +19,7 @@ class Config:
             required=False,
             help="Use Google Routes provider to calculate distances, please set env var {}"
             .format(PROVIDER_KEY_NAME))
+        parser.add_argument("--trucks-csv", required=False, help="The trucks CSV file path")
         self.options = parser.parse_args()
 
     def start(self):
--- src/utils/input.py
+++ src/utils/input.py
@@ -1,5 +1,9 @@
 import csv
 
+from utils.config import config
+
+FILES_PATH_DEFAULT = './../input'
+
 
 def _read_file(path, fields_order=None, skipHeader=True):
     with open(path) as file:
@@ -14,7 +18,7 @@ def _read_file(path, fields_order=None, skipHeader=True):
 
 _files_to_read = {
     'trucks': {
-        'path': './../input/trucks.csv',
+        'path': config.options.trucks_csv if config.options.trucks_csv else '{}/trucks.csv'.format(FILES_PATH_DEFAULT),
         'fields_order': [
             'company',
             'city',
@@ -22,17 +26,17 @@ _files_to_read = {
             'latitude',
             'longitude']},
     'cargos': {
-        'path': './../input/cargo.csv',
-                'fields_order': [
-                    'product',
-                    'origin_city',
-                    'origin_state',
-                    'origin_latitude',
-                    'origin_longitude',
-                    'destination_city',
-                    'destination_state',
-                    'destination_latitude',
-                    'destination_longitude']}}
+        'path': config.options.cargos_csv if config.options.cargos_csv else '{}/cargo.csv'.format(FILES_PATH_DEFAULT),
+        'fields_order': [
+            'product',
+            'origin_city',
+            'origin_state',
+            'origin_latitude',
+            'origin_longitude',
+            'destination_city',
+            'destination_state',
+            'destination_latitude',
+            'destination_longitude']}}
 
 
 def get_trucks_data():
