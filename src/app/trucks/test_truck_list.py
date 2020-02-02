d10236692cf4c5e6acb731387d2f3d727df74f2d
Jota Feldmann <eusoujf@hotmail.com>
2020-02-02 05:45:58 -0300

    Add truck list tests

--- src/app/trucks/test_truck_list.py
+++ src/app/trucks/test_truck_list.py
@@ -2,8 +2,8 @@ from unittest import TestCase
 
 from app.trucks.truck import Truck
 from app.trucks.truck_list import TruckList
-from app.routes.designate_cargo_for_truck import designate_cargo_for_truck
-from tests.utils.utils import get_cargos_fixture, get_trucks_fixture, get_closest_trucks_for_cargo
+from tests.utils.utils import get_trucks_fixture
+from utils.geolocation import GeoPoint
 
 trucks = [Truck(**row) for row in get_trucks_fixture()]
 
@@ -19,3 +19,25 @@ class TestTruckList(TestCase):
         self.assertEqual(len(self.trucks_list), 1)
         self.assertEqual(self.trucks_list.geo_points_list[0], [truck.location.latitude, truck.location.longitude])
         self.assertEqual(len(self.trucks_list.geo_points_list), 1)
+
+    def test_get_closest_trucks_to_location(self):
+        truck_1 = trucks[0]
+        self.trucks_list.append(truck_1)
+
+        truck_2 = trucks[1]
+        self.trucks_list.append(truck_2)
+
+        result_1 = self.trucks_list.get_closest_trucks_to_location(truck_1.location)
+        distance, truck = result_1[0]
+
+        self.assertEqual(len(self.trucks_list), 2)
+        self.assertEqual(len(result_1), 2)
+        self.assertEqual(distance, 0)
+        self.assertEqual(truck, truck_1)
+
+        result_2 = self.trucks_list.get_closest_trucks_to_location(GeoPoint(8, 7))
+        distance, truck = result_2[0]
+
+        self.assertEqual(distance, 9934)
+        self.assertEqual(truck, truck_1)
+        self.assertEqual(result_2[1], (12707, truck_2))
